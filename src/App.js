56e966f5277ed016b3dea3e046581870778ed525
VishnuBathla <87614276+VishnuBathla@users.noreply.github.com>
2023-05-16 12:28:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React is fun
         </a>
       </header>
     </div>
